156621e393b863a978608e3992164890d6ef515b
bernie <mail@javaner.de>
2019-02-24 09:26:40 +0100

    add: defaults for bullet options

--- src/js/stuff/Catapult.js
+++ src/js/stuff/Catapult.js
@@ -6,25 +6,28 @@
 define(["lodash", "three", "ammo"], function(_, THREE, Ammo){
     var defaults = {
         shadow : true,
-        autoLoad : true
+        autoLoad : true,
+        
+        ballMass : 35,
+        ballRadius : 0.4
     };
     
     var catapult = function( VP, PW, opt)
     {
         if ( opt && opt.load ) this.load = opt.load;
-        var scope = this;
-        
+        let scope = this;
+
         this.options = _.extend( {}, defaults, opt );
-        
+
         this.bullet = this.load();
-        
+
         VP.DomEvents.addEventListener( VP.scene, "click", function( obj ) 
         {
-            var ray = VP.raycaster.getRay( obj.origDomEvent );
-            var pos = new THREE.Vector3();
-            var quat = new THREE.Quaternion();
+            let ray = VP.raycaster.getRay( obj.origDomEvent );
+            let pos = new THREE.Vector3();
+            let quat = new THREE.Quaternion();
 
-            var ball = scope.bullet;
+            let ball = scope.bullet;
 
             pos.copy( ray.direction );
             pos.add( ray.origin );
@@ -35,22 +38,21 @@ define(["lodash", "three", "ammo"], function(_, THREE, Ammo){
 
             pos.copy( ray.direction );
             pos.multiplyScalar( 24 );
-            PW.primitivAddPhysic( ball, {mass : 35, velocity : pos} );
-            //createRigidBody( ball, ballShape, ballMass, pos, quat );
+            PW.primitivAddPhysic( ball, {mass : scope.options.ballMass, velocity : pos} );
 
             VP.scene.add( ball );
+            
             if( scope.options.autoLoad ) scope.bullet = scope.load();
         });
     };
     
     catapult.prototype.load = function()
     {
-        var ballMaterial = new THREE.MeshPhongMaterial( { color: 0x202020 } );
-        var ballRadius = 0.4;
-        var ball = new THREE.Mesh( new THREE.SphereGeometry( ballRadius, 14, 10 ), ballMaterial );
+        let ballMaterial = new THREE.MeshPhongMaterial( { color: 0x202020 } );
+        let ball = new THREE.Mesh( new THREE.SphereGeometry( this.options.ballRadius, 14, 10 ), ballMaterial );
         
-        ball.castShadow = this.options.shadow;
-        ball.receiveShadow = this.options.shadow;
+        ball.castShadow     = this.options.shadow;
+        ball.receiveShadow  = this.options.shadow;
         
         return ball;
     };
