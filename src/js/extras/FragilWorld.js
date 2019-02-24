8d3c2554039d8c21479ac612f5da08377704ae73
bernie <mail@javaner.de>
2019-02-24 09:31:21 +0100

    change: extend prototype

--- src/js/extras/FragilWorld.js
+++ src/js/extras/FragilWorld.js
@@ -15,11 +15,11 @@ define(["lodash", "ammo", "three", "vendor/threejs/extras/ConvexObjectBreaker"],
     let objectsToRemove = [];
     let numObjectsToRemove = 0;
     
-    let FragilWorld = function( VP, PW, opt ){
+    let FragilWorld = function( PW, opt ){
         
         this.options = _.extend( {}, defaults, opt );
         this.PW = PW;
-        this.VP = VP;
+        this.VP = PW.VP;
         
         this.convexBreaker = new THREE.ConvexObjectBreaker();
         
@@ -29,37 +29,29 @@ define(["lodash", "ammo", "three", "vendor/threejs/extras/ConvexObjectBreaker"],
         for ( let i = 0; i < 500; i++ ) {
 		objectsToRemove[ i ] = null;
         }
+        updatePhysics = function(){ 
+            scope._updatePhysics.apply( scope, arguments ); 
+        }; 
         
-        this.updatePhysics = function(){ scope._updatePhysics.apply(scope, arguments); }; 
-        
-        this.onPrimitivAddPhysic = function( obj ){
-            if ( obj.userData.breakable ) this.prepareBreakableObject( obj );
-        };
-        this.onAdd = function(){
-            
-        };
-        
-        this.prepareBreakableObject = function( obj ){
-            this.convexBreaker.prepareBreakableObject( obj, 1000, new THREE.Vector3(), new THREE.Vector3(), true );
-            
-            var btVecUserData = new Ammo.btVector3( 0, 0, 0 );
-            btVecUserData.threeObject = obj;
-            obj._physiBody.setUserPointer( btVecUserData );
-        };
         
         function removeDebris( object ) {
-            VP.scene.remove( object );
+            scope.VP.scene.remove( object );
 	}
         
-        function createDebrisFromBreakableObject( obj )
+        function createDebrisFromBreakableObject( obj, opt )
         {
-            obj.castShadow = true;
-            obj.receiveShadow = true;
+            let options = _.extend({
+                castShadow : true,
+                receiveShadow : true
+            }, opt );
+            
+            obj.castShadow = options.castShadow;
+            obj.receiveShadow = options.castShadow;
 
-            PW.primitivAddPhysic( obj );
+            scope.PW.primitivAddPhysic( obj, options );
 
             // Set pointer back to the three object only in the debris objects
-            var btVecUserData = new Ammo.btVector3( 0, 0, 0 );
+            let btVecUserData = new Ammo.btVector3( 0, 0, 0 );
             btVecUserData.threeObject = obj;
             obj._physiBody.setUserPointer( btVecUserData );
         }
@@ -71,8 +63,8 @@ define(["lodash", "ammo", "three", "vendor/threejs/extras/ConvexObjectBreaker"],
                 let contactManifold = this.PW.dispatcher.getManifoldByIndexInternal( i );
                 let rb0 = contactManifold.getBody0();
                 let rb1 = contactManifold.getBody1();
-                var threeObject0 = Ammo.castObject( rb0.getUserPointer(), Ammo.btVector3 ).threeObject;
-                var threeObject1 = Ammo.castObject( rb1.getUserPointer(), Ammo.btVector3 ).threeObject;
+                let threeObject0 = Ammo.castObject( rb0.getUserPointer(), Ammo.btVector3 ).threeObject;
+                let threeObject1 = Ammo.castObject( rb1.getUserPointer(), Ammo.btVector3 ).threeObject;
                     
                 if ( ! threeObject0 && ! threeObject1 ) {
                     continue;
@@ -85,62 +77,71 @@ define(["lodash", "ammo", "three", "vendor/threejs/extras/ConvexObjectBreaker"],
                 var collided0  = userData0 ?    userData0.collided      : false;
                 var collided1  = userData1 ?    userData1.collided      : false;
 
-                    if ( ( ! breakable0 && ! breakable1 ) || ( collided0 && collided1 ) ) {
-                        continue;
-                    }
+                if ( ( ! breakable0 && ! breakable1 ) || ( collided0 && collided1 ) ) {
+                    continue;
+                }
 
-                    var contact = false;
-                    var maxImpulse = 0;
+                let contact = false;
+                let maxImpulse = 0;
                     
                     for ( let j = 0, jl = contactManifold.getNumContacts(); j < jl; j ++ ) {
-                        var contactPoint = contactManifold.getContactPoint( j );
+                        let contactPoint = contactManifold.getContactPoint( j );
+                        
                         if ( contactPoint.getDistance() < 0 ) {
                             contact = true;
-                            var impulse = contactPoint.getAppliedImpulse();
+                            let impulse = contactPoint.getAppliedImpulse();
                             if ( impulse > maxImpulse ) {
                                 maxImpulse = impulse;
-                                var pos = contactPoint.get_m_positionWorldOnB();
-                                var normal = contactPoint.get_m_normalWorldOnB();
+                                let pos = contactPoint.get_m_positionWorldOnB();
+                                let normal = contactPoint.get_m_normalWorldOnB();
                                 impactPoint.set( pos.x(), pos.y(), pos.z() );
                                 impactNormal.set( normal.x(), normal.y(), normal.z() );
                             }
                             break;
                         }
                     }
-                    // If no point has contact, abort
-                    if ( ! contact ) {
-                        continue;
+                // If no point has contact, abort
+                if ( ! contact ) {
+                    continue;
+                }
+
+                // Subdivision
+                let fractureImpulse = 250;
+
+                if ( breakable0 && !collided0 && maxImpulse > fractureImpulse ) 
+                {
+                    let debris = this.convexBreaker.subdivideByImpact( threeObject0, impactPoint, impactNormal , 1, 2, 1.5 );
+
+                    let numObjects = debris.length;
+
+                    for ( let j = 0; j < numObjects; j++ ) {
+                        debris[ j ].geometry.computeBoundingBox();
+                        debris[ j ].geometry.computeBoundingSphere ();
+                        createDebrisFromBreakableObject( debris[ j ], {density : userData0.ammo_density || 1} );
+                        this.VP.scene.add( debris[ j ] );
                     }
 
-                    // Subdivision
-                    var fractureImpulse = 250;
-                    if ( breakable0 && !collided0 && maxImpulse > fractureImpulse ) {
-                        var debris = this.convexBreaker.subdivideByImpact( threeObject0, impactPoint, impactNormal , 1, 2, 1.5 );
+                    objectsToRemove[ numObjectsToRemove++ ] = threeObject0;
+                    userData0.collided = true;
+                }
+                    
+                if ( breakable1 && !collided1 && maxImpulse > fractureImpulse ) 
+                {
+                    let debris = this.convexBreaker.subdivideByImpact( threeObject1, impactPoint, impactNormal , 1, 2, 1.5 );
+                    let numObjects = debris.length;
 
-                        var numObjects = debris.length;
-                        for ( var j = 0; j < numObjects; j++ ) {
-                            debris[ j ].geometry.computeBoundingBox();
-                            debris[ j ].geometry.computeBoundingSphere ();
-                            createDebrisFromBreakableObject( debris[ j ] );
-                            this.VP.scene.add( debris[ j ] );
-                        }
-                        objectsToRemove[ numObjectsToRemove++ ] = threeObject0;
-                        userData0.collided = true;
+                    for ( let j = 0; j < numObjects; j++ ) {
+                        debris[ j ].geometry.computeBoundingBox();
+                        debris[ j ].geometry.computeBoundingSphere ();
+                        createDebrisFromBreakableObject( debris[ j ] );
+                        this.VP.scene.add( debris[ j ] );
                     }
-                    if ( breakable1 && !collided1 && maxImpulse > fractureImpulse ) {
-                        var debris = this.convexBreaker.subdivideByImpact( threeObject1, impactPoint, impactNormal , 1, 2, 1.5 );
-                        var numObjects = debris.length;
-                        for ( var j = 0; j < numObjects; j++ ) {
-                            debris[ j ].geometry.computeBoundingBox();
-                            debris[ j ].geometry.computeBoundingSphere ();
-                            createDebrisFromBreakableObject( debris[ j ] );
-                            this.VP.scene.add( debris[ j ] );
-                        }
-                        objectsToRemove[ numObjectsToRemove++ ] = threeObject1;
-                        userData1.collided = true;
-                    }
+
+                    objectsToRemove[ numObjectsToRemove++ ] = threeObject1;
+                    userData1.collided = true;
+                }
             }
-            for ( var i = 0; i < numObjectsToRemove; i++ ) {
+            for ( let i = 0; i < numObjectsToRemove; i++ ) {
                 removeDebris( objectsToRemove[ i ] );
             }
             numObjectsToRemove = 0;
@@ -148,6 +149,23 @@ define(["lodash", "ammo", "three", "vendor/threejs/extras/ConvexObjectBreaker"],
     };
     
     FragilWorld.name = "FragilWorld";
+    
+    FragilWorld.prototype = _.create( FragilWorld.prototype, {
+        
+        onPrimitivAddPhysic : function( obj ){
+            if ( obj.userData.breakable ) this.prepareBreakableObject( obj );
+        },
+        onAdd : function(){
+            
+        },
+        prepareBreakableObject : function( obj ){
+            this.convexBreaker.prepareBreakableObject( obj, 1000, new THREE.Vector3(), new THREE.Vector3(), true );
+            
+            var btVecUserData = new Ammo.btVector3( 0, 0, 0 );
+            btVecUserData.threeObject = obj;
+            obj._physiBody.setUserPointer( btVecUserData );
+        }
+    });
     
     return FragilWorld;
     
